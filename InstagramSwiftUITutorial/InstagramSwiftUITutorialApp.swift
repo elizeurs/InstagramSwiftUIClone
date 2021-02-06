92cf982139b4e924235cda3398924b405af8f4a8
elizeurs <13ders@gmail.com>
2021-02-06 17:23:55 -0300

    Initial Commit

---
+++ InstagramSwiftUITutorial/InstagramSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUITutorialApp.swift
+//  InstagramSwiftUITutorial
+//
+//  Created by Elizeu RS on 06/02/21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
