e94365357b14a65966c95bfa1c067c0f3595f14b
elizeurs <13ders@gmail.com>
2021-02-11 15:34:26 -0300

    Building The Post Grid View

---
+++ InstagramSwiftUITutorial/View/Items/PostGridView.swift
@@ -0,0 +1,31 @@
+//
+//  PostGridView.swift
+//  InstagramSwiftUITutorial
+//
+//  Created by Elizeu RS on 11/02/21.
+//
+
+import SwiftUI
+
+struct PostGridView: View {
+  private let items = [GridItem(), GridItem(), GridItem()]
+  private let width = UIScreen.main.bounds.width / 3
+  
+    var body: some View {
+      LazyVGrid (columns: items, spacing: 2, content: {
+        ForEach(0 ..< 10) { _ in
+          Image("batman")
+            .resizable()
+            .scaledToFill()
+            .frame(width: width, height: width)
+            .clipped()
+        }
+      })
+    }
+}
+
+struct PostGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostGridView()
+    }
+}
