0fc982dc59b69ab92a9258d04cb0a0fd1df8c14a
elizeurs <13ders@gmail.com>
2021-02-11 20:14:13 -0300

    Building The Notification UI

---
+++ InstagramSwiftUITutorial/View/Notifications/NotificationCell.swift
@@ -0,0 +1,52 @@
+//
+//  NotificationCell.swift
+//  InstagramSwiftUITutorial
+//
+//  Created by Elizeu RS on 11/02/21.
+//
+
+import SwiftUI
+
+struct NotificationCell: View {
+  @State private var showPostImage = true
+  
+  var body: some View {
+    
+    HStack {
+      Image("batman")
+        .resizable()
+        .scaledToFill()
+        .frame(width: 40, height: 40)
+        .clipShape(Circle())
+      
+      Text("batman").font(.system(size: 14, weight: .semibold)) +
+        Text(" liked one of your posts.")
+        .font(.system(size: 15))
+      
+      Spacer()
+      
+      if showPostImage {
+        Image("batman")
+          .resizable()
+          .scaledToFill()
+          .frame(width: 40, height: 40)
+      } else {
+        Button(action: {}, label: {
+          Text("Follow")
+            .padding(.horizontal, 20)
+            .padding(.vertical, 8)
+            .background(Color(.systemBlue))
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+            .font(.system(size: 14, weight: .semibold))
+        })
+      }
+    }.padding(.horizontal)
+  }
+}
+
+struct NotificationCell_Previews: PreviewProvider {
+  static var previews: some View {
+    NotificationCell()
+  }
+}
