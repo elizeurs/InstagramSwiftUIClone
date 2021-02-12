a3a68e0e26886b6a13ba56108bb43ff0cf2f3eab
elizeurs <13ders@gmail.com>
2021-02-12 19:50:01 -0300

    Building the User Interface

--- InstagramSwiftUITutorial/View/UploadPost/UploadPostView.swift
+++ InstagramSwiftUITutorial/View/UploadPost/UploadPostView.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct UploadPostView: View {
-    var body: some View {
-        Text("Upload Post")
+  @State private var selectedImage: UIImage?
+  @State var postImage: Image?
+  @State var captionText = ""
+  
+  var body: some View {
+    //        Text("Upload Post")
+    VStack {
+      if postImage != nil {
+        Button(action: {}, label: {
+          Image("plus_photo")
+            .resizable()
+            .renderingMode(.template)
+            .scaledToFill()
+            .frame(width: 180, height: 180)
+            .clipped()
+            .padding(.top, 56)
+            .foregroundColor(.black)
+        })
+      } else {
+        HStack(alignment: .top) {
+          Image("batman")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 96, height: 96)
+            .clipped()
+          
+          TextField("Enter your caption..", text: $captionText)
+        }.padding()
+        
+        Button(action: {}, label: {
+          Text("Share")
+            .font(.system(size: 16, weight: .semibold))
+            .frame(width: 360, height: 50)
+            .background(Color.blue)
+            .cornerRadius(5)
+            .foregroundColor(.white)
+        }).padding()
+      }
+      
+      Spacer()
+      
     }
+  }
 }
 
 struct UploadPostView_Previews: PreviewProvider {
-    static var previews: some View {
-        UploadPostView()
-    }
+  static var previews: some View {
+    UploadPostView()
+  }
 }
