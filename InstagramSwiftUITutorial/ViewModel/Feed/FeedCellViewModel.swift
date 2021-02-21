839486658ca875e1f8ae91f7c1aebd6f76a3d9eb
elizeurs <13ders@gmail.com>
2021-02-21 17:57:34 -0300

    Unliking & Check Post For Like

--- InstagramSwiftUITutorial/ViewModel/Feed/FeedCellViewModel.swift
+++ InstagramSwiftUITutorial/ViewModel/Feed/FeedCellViewModel.swift
@@ -17,31 +17,52 @@ class FeedCellViewModel: ObservableObject {
   
   init(post: Post) {
     self.post = post
+    checkIfUserLikedPost()
   }
   
   func like() {
-//    print("Like post")
+    //    print("Like post")
     guard let uid = AuthViewModel.shared.userSession?.uid else { return }
     guard let postId = post.id else { return }
     
     COLLECTION_POSTS.document(postId).collection("post-likes")
       .document(uid).setData([:]) { _ in
-      COLLECTION_USERS.document(uid).collection("user-likes")
-        .document(postId).setData([:]) { _ in
-          
-          COLLECTION_POSTS.document(postId).updateData(["likes": self.post.likes + 1])
-          
-        self.post.didLike = true
-          self.post.likes += 1
+        COLLECTION_USERS.document(uid).collection("user-likes")
+          .document(postId).setData([:]) { _ in
+            
+            COLLECTION_POSTS.document(postId).updateData(["likes": self.post.likes + 1])
+            
+            self.post.didLike = true
+            self.post.likes += 1
+          }
       }
-    }
   }
   
   func unlike() {
-    print("Unlike post")
+    //    print("Unlike post")
+    guard post.likes > 0 else { return }
+    guard let uid = AuthViewModel.shared.userSession?.uid else { return }
+    guard let postId = post.id else { return }
+    
+    COLLECTION_POSTS.document(postId).collection("post-likes").document(uid).delete { _ in
+      COLLECTION_USERS.document(uid).collection("user-likes").document(postId).delete { _ in
+        COLLECTION_POSTS.document(postId).updateData(["likes": self.post.likes - 1])
+        
+        self.post.didLike = false
+        self.post.likes -= 1
+      }
+    }
+    
   }
   
   func checkIfUserLikedPost() {
+    guard let uid = AuthViewModel.shared.userSession?.uid else { return }
+    guard let postId = post.id else { return }
     
+    COLLECTION_USERS.document(uid).collection("user-likes").document(postId)
+      .getDocument { snapshot, _ in
+        guard let didLike = snapshot?.exists else { return }
+        self.post.didLike = didLike
+      }
   }
 }
