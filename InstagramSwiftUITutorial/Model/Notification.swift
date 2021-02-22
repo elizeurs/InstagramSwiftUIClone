d5384275ecc4f7309f64e24d5105bb9f935ec91c
elizeurs <13ders@gmail.com>
2021-02-22 18:56:13 -0300

    Notifications Overview & Data Model

---
+++ InstagramSwiftUITutorial/Model/Notification.swift
@@ -0,0 +1,34 @@
+//
+//  Notification.swift
+//  InstagramSwiftUITutorial
+//
+//  Created by Elizeu RS on 22/02/21.
+//
+
+import FirebaseFirestoreSwift
+import Firebase
+
+struct Notification: Identifiable, Decodable {
+  @DocumentID var id: String?
+  var postId: String?
+  let username: String
+  let profileImageUrl: String
+  let timestamp: Timestamp
+  let type: Int
+  let uid: String
+}
+
+enum NotificationType: Int, Decodable {
+  case like
+  case comment
+  case follow
+  
+  var notificationMessage: String {
+    switch self {
+    case .like: return " liked one of your posts."
+    case .comment: return " commented on one of your posts."
+    case .follow: return " started following you."
+
+    }
+  }
+}
