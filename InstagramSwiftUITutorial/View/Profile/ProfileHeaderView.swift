fb104857162311f0ee4ecd525b3e5b61f22df9a6
elizeurs <13ders@gmail.com>
2021-02-25 19:34:01 -0300

    User Stats (Posts, Followers, Following)

--- InstagramSwiftUITutorial/View/Profile/ProfileHeaderView.swift
+++ InstagramSwiftUITutorial/View/Profile/ProfileHeaderView.swift
@@ -24,10 +24,24 @@ struct ProfileHeaderView: View {
         
         Spacer()
         
+//        HStack(spacing: 16) {
+//          UserStatView(value: 1, title: "Post")
+//          UserStatView(value: 2, title: "Followers")
+//          UserStatView(value: 4, title: "Following")
+//        }.padding(.trailing, 32)
+        
         HStack(spacing: 16) {
-          UserStatView(value: 1, title: "Post")
-          UserStatView(value: 2, title: "Followers")
-          UserStatView(value: 4, title: "Following")
+          
+//          if let stats = viewModel.user.stats {
+//            UserStatView(value: stats.posts, title: "Post")
+//            UserStatView(value: stats.followers, title: "Followers")
+//            UserStatView(value: stats.following, title: "Following")
+//          }
+          
+          UserStatView(value: viewModel.user.stats?.posts ?? 0, title: "Post")
+          UserStatView(value: viewModel.user.stats?.followers ?? 0, title: "Followers")
+          UserStatView(value: viewModel.user.stats?.following ?? 0, title: "Following")
+          
         }.padding(.trailing, 32)
       }
       
